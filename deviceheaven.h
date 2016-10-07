ca16dede30b228b1297c5a32cfdb8fe567804112
Device Heaven <matheus@weevee.com.br>
2016-10-07 15:21:34 -0300

    Update deviceheaven.h

--- deviceheaven.h
+++ deviceheaven.h
@@ -2,6 +2,8 @@
    Compatível com Arduino UNO, MEGA, DUE... */
 #include <Arduino.h>
 #include <string.h>
+#include <Ethernet.h>
+#include <SPI.h>
 
 void connectWiFi(String SSID, String PASS);
 
@@ -9,4 +11,11 @@ String ComandoAT(String comando, const int timeout);
  
 void send_to_DH(String TOKEN, String LEITURA_ID, String LEITURA);
 
+void send_to_DH2(String TOKEN, String LEITURA_ID, String LEITURA);
+
+void Ethernet_start(uint8_t *mac);
+
+void Ethernet_start(uint8_t *mac, IPAddress ip);
+
 void Serial2_start();
+
